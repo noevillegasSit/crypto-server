8856a8e81301758ebe2d49f8c55de7e0b1b20782
Noe Villegas <noe.villegas333@gmail.com>
2018-12-05 13:39:36 -0600

    Other port

--- index.js
+++ index.js
@@ -108,9 +108,6 @@ app.get('/api/basic', (req, res) => {
   
 });
 
-const PORT = process.env.PORT || 5000
-
-
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
