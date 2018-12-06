eda402e4eafdc1c6266ba08c55d99f334961b536
Noe Villegas <noe.villegas333@gmail.com>
2018-12-06 16:22:37 -0600

    Add allowCrossDomain

--- index.js
+++ index.js
@@ -51,6 +51,13 @@ const app = express();
 app.use(allowCrossDomain);
 app.use(express.static(path.join(__dirname,"public")));
 
+app.all('*', function(req, res, next) {
+  var origin = req.get('origin'); 
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 // Latest all crypto
 app.get('/api/latest', (req, res) => {
   rp(requestLatest).then(response => {
